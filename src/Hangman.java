f27edc00cbfdfad8c283c8ccb88ff3f1094e8883
wangfeng <578627759@qq.com>
2016-07-04 22:42:23 +0800

    添加了判断正误的功能

--- src/Hangman.java
+++ src/Hangman.java
@@ -4,14 +4,55 @@
 public class Hangman {
     private String str;
     private int lenth;
+    private String guess_str = "aeiou";
+    private int times;
 
     public Hangman(String str) {
         this.str = str;
+        times = str.length() + 1;
     }
 
 
     public int getLenth() {
-        lenth = str.length();
-        return lenth;
+        return  str.length();
     }
+
+    public  String show() {
+
+        return "a___e";
+    }
+
+    public int getTime() {
+        return times;
+    }
+
+    public  String Yuan() {
+        return "aeiou";
+    }
+
+    public String showWords() {
+        char st[] = str.toCharArray();
+        String str_buff = "";
+        for(int i = 0;i < str.length();i++){
+            str_buff += isContains(st[i]) ? Character.valueOf(st[i]) : "_";
+        }
+        return str_buff;
+    }
+
+    private boolean isContains(char c) {
+        return guess_str.indexOf(String.valueOf(c)) != -1;
+    }
+
+    public String guess(String guess_char) {
+        guess_str += guess_char;
+        times--;
+        return guess_str;
+    }
+
+    public String result() {
+        System.out.println(showWords());
+        return (times > 0 && str.equals(showWords())) ?  "you win !" :  "you lose !";
+    }
+
+
 }
--- src/HangmanTest.java
+++ src/HangmanTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -8,20 +9,84 @@ import static org.junit.Assert.*;
  */
 public class HangmanTest {
     @Test
-    public void Should_get_long_when_get_a_word(){
-        String str = "apple";
-        Hangman hangman = new Hangman(str);
-        int len = str.length();
+    public void Should_get_all_when_the_word_is_apple(){
+        Hangman hangman = new Hangman("apple");
 
-        assertThat(hangman.getLenth(),is(len));
+
+        assertThat(hangman.getLenth(),is(5));
+        assertThat(hangman.getTime(),is(6));
+        assertThat(hangman.Yuan(),is("aeiou"));
+        assertThat(hangman.showWords(),is("a___e"));
+    }
+    @Test
+    public void Should_get_all_when_the_word_is_orange(){
+        Hangman hangman = new Hangman("orange");
+
+
+        assertThat(hangman.getLenth(),is(6));
+        assertThat(hangman.getTime(),is(7));
+        assertThat(hangman.Yuan(),is("aeiou"));
+        assertThat(hangman.showWords(),is("o_a__e"));
+    }
+
+    @Test
+    public void should_true_when_guess_words_is_r(){
+        Hangman hangman = new Hangman("orange");
+
+
+        assertThat(hangman.getLenth(),is(6));
+        assertThat(hangman.getTime(),is(7));
+        assertThat(hangman.guess("r"),is("aeiour"));
+        assertThat(hangman.Yuan(),is("aeiou"));
+        assertThat(hangman.showWords(),is("ora__e"));
+    }
+
+    @Test
+    public void should_flase_when_guess_words_is_h(){
+        Hangman hangman = new Hangman("orange");
+
+
+        assertThat(hangman.getLenth(),is(6));
+        assertThat(hangman.getTime(),is(7));
+        assertThat(hangman.guess("h"),is("aeiouh"));
+        assertThat(hangman.Yuan(),is("aeiou"));
+        assertThat(hangman.showWords(),is("o_a__e"));
     }
 
 
     @Test
-    public void Should_get_6_when_inout_apples(){
-        Hangman hangman = new Hangman("apples");
-        assertThat(hangman.getLenth(),is(6));
+    public void should_times_cut_1_when_guess_a_char(){
+        Hangman hangman = new Hangman("orange");
+
+        assertThat(hangman.getTime(),is(7));
+        assertThat(hangman.guess("h"),is("aeiouh"));
+        assertThat(hangman.getTime(),is(6));
+        assertThat(hangman.Yuan(),is("aeiou"));
+        assertThat(hangman.showWords(),is("o_a__e"));
+    }
+
+    @Test
+    public void should_false_when_times_use_up(){
+        Hangman hangman = new Hangman("orange");
+
+        for(int i = 0;i < 7;i++){
+            hangman.guess("h");
+        }
+        assertThat(hangman.result(),is("you lose !"));
 
     }
+
+    @Test
+    public void should_true_when_guess_the_word(){
+        Hangman hangman = new Hangman("orange");
+
+        hangman.guess("r");
+        hangman.guess("n");
+        hangman.guess("g");
+        assertThat(hangman.getTime(),is(4));
+        assertThat(hangman.showWords(),is("orange"));
+        assertThat(hangman.result(),is("you win !"));
+    }
+
 
 }
