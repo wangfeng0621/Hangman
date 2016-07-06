71a69d66204e87627ea246252fe32880a370ed9e
wangfeng <578627759@qq.com>
2016-07-06 13:33:14 +0800

    完成了基本功能

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,58 +1,59 @@
+import java.util.Scanner;
+
 /**
  * Created by feng on 2016/7/2.
  */
 public class Hangman {
-    private String str;
-    private int lenth;
-    private String guess_str = "aeiou";
-    private int times;
 
-    public Hangman(String str) {
-        this.str = str;
-        times = str.length() + 1;
+    public int str_length;
+    public int times;
+    public String guess_str;
+    public String show_str;
+    public String str;
+
+    public Hangman(String s) {
+        str = s;
+        str_length = str.length();
+        times = str_length + 1;
+        guess_str = "iaoue";
+        show_the_guessed_str();
     }
 
-
-    public int getLenth() {
-        return  str.length();
-    }
-
-    public  String show() {
-
-        return "a___e";
-    }
-
-    public int getTime() {
-        return times;
-    }
-
-    public  String Yuan() {
-        return "aeiou";
-    }
-
-    public String showWords() {
+    private void show_the_guessed_str() {
         char st[] = str.toCharArray();
-        String str_buff = "";
-        for(int i = 0;i < str.length();i++){
-            str_buff += isContains(st[i]) ? Character.valueOf(st[i]) : "_";
-        }
-        return str_buff;
+        show_str = "";
+        for(int i = 0;i < str_length;i++)
+           show_str += isContain(st[i]) ?  st[i]:"_";
     }
 
-    private boolean isContains(char c) {
-        return guess_str.indexOf(String.valueOf(c)) != -1;
+    private boolean isContain(char c) {
+        return (guess_str.indexOf(c) == -1) ? false:true;
     }
 
-    public String guess(String guess_char) {
-        guess_str += guess_char;
+
+    public void guess_a_char(char c) {
+        if(isContain(c))
+            return;
+        guess_str += c;
+        show_the_guessed_str();
         times--;
-        return guess_str;
     }
 
-    public String result() {
-        System.out.println(showWords());
-        return (times > 0 && str.equals(showWords())) ?  "you win !" :  "you lose !";
+
+    public boolean check_times() {
+        return times > 0;
     }
+
+    public boolean chenk_answer() {
+        return (check_times() && str.equals(show_str));
+    }
+
+    public void show_status() {
+        System.out.println("剩余次数："+times);
+        System.out.println("现在的猜出的字符所组成的单词："+show_str);
+        System.out.println("请继续！！！");
+    }
+
 
 
 }
--- src/HangmanTest.java
+++ src/HangmanTest.java
@@ -1,6 +1,5 @@
+
 import org.junit.Test;
-
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
@@ -9,84 +8,131 @@ import static org.junit.Assert.*;
  */
 public class HangmanTest {
     @Test
-    public void Should_get_all_when_the_word_is_apple(){
+    public void show_shuxin_when_word_is_apple(){
         Hangman hangman = new Hangman("apple");
 
-
-        assertThat(hangman.getLenth(),is(5));
-        assertThat(hangman.getTime(),is(6));
-        assertThat(hangman.Yuan(),is("aeiou"));
-        assertThat(hangman.showWords(),is("a___e"));
-    }
-    @Test
-    public void Should_get_all_when_the_word_is_orange(){
-        Hangman hangman = new Hangman("orange");
-
-
-        assertThat(hangman.getLenth(),is(6));
-        assertThat(hangman.getTime(),is(7));
-        assertThat(hangman.Yuan(),is("aeiou"));
-        assertThat(hangman.showWords(),is("o_a__e"));
-    }
-
-    @Test
-    public void should_true_when_guess_words_is_r(){
-        Hangman hangman = new Hangman("orange");
-
-
-        assertThat(hangman.getLenth(),is(6));
-        assertThat(hangman.getTime(),is(7));
-        assertThat(hangman.guess("r"),is("aeiour"));
-        assertThat(hangman.Yuan(),is("aeiou"));
-        assertThat(hangman.showWords(),is("ora__e"));
-    }
-
-    @Test
-    public void should_flase_when_guess_words_is_h(){
-        Hangman hangman = new Hangman("orange");
-
-
-        assertThat(hangman.getLenth(),is(6));
-        assertThat(hangman.getTime(),is(7));
-        assertThat(hangman.guess("h"),is("aeiouh"));
-        assertThat(hangman.Yuan(),is("aeiou"));
-        assertThat(hangman.showWords(),is("o_a__e"));
-    }
-
-
-    @Test
-    public void should_times_cut_1_when_guess_a_char(){
-        Hangman hangman = new Hangman("orange");
-
-        assertThat(hangman.getTime(),is(7));
-        assertThat(hangman.guess("h"),is("aeiouh"));
-        assertThat(hangman.getTime(),is(6));
-        assertThat(hangman.Yuan(),is("aeiou"));
-        assertThat(hangman.showWords(),is("o_a__e"));
-    }
-
-    @Test
-    public void should_false_when_times_use_up(){
-        Hangman hangman = new Hangman("orange");
-
-        for(int i = 0;i < 7;i++){
-            hangman.guess("h");
-        }
-        assertThat(hangman.result(),is("you lose !"));
+        assertThat(hangman.str,is("apple"));
+        assertThat(hangman.str_length,is(5));
+        assertThat(hangman.times,is(6));
+        assertThat(hangman.guess_str,is("iaoue"));
+        assertThat(hangman.show_str,is("a___e"));
 
     }
 
     @Test
-    public void should_true_when_guess_the_word(){
-        Hangman hangman = new Hangman("orange");
+    public void show_shuxin_when_word_is_application(){
+        Hangman hangman = new Hangman("application");
 
-        hangman.guess("r");
-        hangman.guess("n");
-        hangman.guess("g");
-        assertThat(hangman.getTime(),is(4));
-        assertThat(hangman.showWords(),is("orange"));
-        assertThat(hangman.result(),is("you win !"));
+        assertThat(hangman.str,is("application"));
+        assertThat(hangman.str_length,is(11));
+        assertThat(hangman.times,is(12));
+        assertThat(hangman.guess_str,is("iaoue"));
+        assertThat(hangman.show_str,is("a___i_a_io_"));
     }
 
+
+    @Test
+    public void  show_guess_word_when_guess_char_is_p(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        assertThat(hangman.times,is(5));
+        assertThat(hangman.guess_str,is("iaouep"));
+        assertThat(hangman.show_str,is("app_e"));
+    }
+
+    @Test
+    public void show_false_when_times_out_put(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.guess_a_char('w');
+        hangman.guess_a_char('r');
+        hangman.guess_a_char('t');
+        hangman.guess_a_char('y');
+        hangman.guess_a_char('s');
+        assertThat(hangman.check_times(),is(false));
+    }
+
+    @Test
+    public void show_shuxin_when_guess_the_same_char(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.guess_a_char('p');
+        assertThat(hangman.times,is(5));
+        assertThat(hangman.guess_str,is("iaouep"));
+        assertThat(hangman.show_str,is("app_e"));
+    }
+
+    @Test
+    public void show_true_when_guess_right_the_word(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.guess_a_char('l');
+        assertThat(hangman.guess_str,is("iaouepl"));
+        assertThat(hangman.show_str,is("apple"));
+        assertThat(hangman.chenk_answer(),is(true));
+    }
+
+    @Test
+    public void show_false_when_guess_wrong_the_word(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.guess_a_char('q');
+        hangman.guess_a_char('w');
+        hangman.guess_a_char('r');
+        hangman.guess_a_char('t');
+        hangman.guess_a_char('y');
+        assertThat(hangman.chenk_answer(),is(false));
+    }
+
+    @Test
+    public void show_status_when_guess_a_char(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.show_status();
+    }
+
+    @Test
+    public void show_answeer_when_guess_right_the_word(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.show_status();
+        hangman.guess_a_char('l');
+        hangman.show_status();
+
+        if(hangman.chenk_answer())
+            System.out.println("YOU WIN !!! GAME OVER");
+        else
+            System.out.println("YOU LOSE !!! GAME OVER");
+    }
+
+    @Test
+    public void show_answer_when_guess_wrong_the_word(){
+        Hangman hangman = new Hangman("apple");
+
+        hangman.guess_a_char('p');
+        hangman.show_status();
+        hangman.guess_a_char('q');
+        hangman.show_status();
+        hangman.guess_a_char('w');
+        hangman.show_status();
+        hangman.guess_a_char('r');
+        hangman.show_status();
+        hangman.guess_a_char('t');
+        hangman.show_status();
+        hangman.guess_a_char('y');
+        hangman.show_status();
+
+        if(hangman.chenk_answer())
+            System.out.println("YOU WIN !!! GAME OVER");
+        else
+            System.out.println("YOU LOSE !!! GAME OVER");
+    }
 
 }
